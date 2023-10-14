a96226500d8f07f52d678ce1e3c7db91a5450448
unknown <jonaccomplish2@gmail.com>
2023-10-14 12:57:43 +0000

    Extract String

--- app/src/main/java/com/example/learntogetherapp/MainActivity.kt
+++ app/src/main/java/com/example/learntogetherapp/MainActivity.kt
@@ -15,11 +15,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.learntogetherapp.ui.theme.LearnTogetherAppTheme
+
+/**
+ * Created by Jonas 10/10/2023
+ */
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,9 +37,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     LearnTogether(
-                        messageHeader = "Jetpack Compose tutorial",
-                        messageIntro = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-                        messageBody = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
+                        messageHeader = stringResource(R.string.message_header),
+                        messageIntro = stringResource(R.string.message_intro),
+                        messageBody = stringResource(R.string.message_body)
                     )
                 }
             }
@@ -42,13 +47,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Composable
 fun LearnTogether(
@@ -86,12 +84,5 @@ fun LearnTogether(
 
     }
 
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnTogetherAppTheme {
-        Greeting("Android")
-    }
 }
